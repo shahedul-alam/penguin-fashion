2bafbc561475a6852e3cb201d5eceec310e1a65b
shahedul-alam <shaheedalamcontact@gmail.com>
2024-08-11 17:39:40 +0600

    header section added for both mobile and large device

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'roboto': ["Roboto", "sans-serif"],
+        'bebas': ["Bebas Neue", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
